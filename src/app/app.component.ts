b185b3fd59cb421cce99f0642da18fa59fb5c007
Daniele Ferreira Dias <danniele-diias@hotmail.com.br>
2020-12-01 09:03:39 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-import { PetService } from './services/pet.service';
-import { Pet } from './models/pet';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { PetService } from "./services/pet.service";
+import { Pet } from "./models/pet";
+import { Breed } from "./models/breed";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
+    pet = {} as Pet;
+    pets: Pet[] = [];
+    breeds: Breed[] = [];
 
-  pet = {} as Pet;
-  pets: Pet[] = [];
+    constructor(private petService: PetService) {}
 
-  constructor(private petService: PetService) {}
-  
-  ngOnInit() {
-    this.getPets();
-  }
-
-  savePet(form: NgForm) {
-    if (this.pet.id !== undefined) {
-      this.petService.updatePet(this.pet).subscribe(() => {
-        this.cleanForm(form);
-      });
-    } else {
-      this.petService.savePet(this.pet).subscribe(() => {
-        this.cleanForm(form);
-      });
+    ngOnInit() {
+        this.getPets();
+        this.getBreeds();
     }
-  }
 
-  getPets() {
-    this.petService.getPets().subscribe((pets: Pet[]) => {
-      this.pets = pets;
-    });
-  }
+    savePet(form: NgForm) {
+        if (this.pet.id !== undefined) {
+            this.petService.updatePet(this.pet).subscribe(() => {
+                this.cleanForm(form);
+            });
+        } else {
+            this.petService.savePet(this.pet).subscribe(() => {
+                this.cleanForm(form);
+            });
+        }
+    }
 
-  deletePet(pet: Pet) {
-    
-    this.petService.deletePet(pet).subscribe(() => {
-      this.getPets();
-    });
-  }
+    getPets() {
+        this.petService.getPets().subscribe((pets: Pet[]) => {
+            this.pets = pets;
+        });
+    }
 
-  editPet(pet: Pet) {
-    this.pet = { ...pet };
-  }
+    getBreeds() {
+        this.petService.getBreeds().subscribe((breeds: Breed[]) => {
+            this.breeds = breeds;
+        });
+    }
 
-  cleanForm(form: NgForm) {
-    this.getPets();
-    form.resetForm();
-    this.pet = {} as Pet;
-  }
+    deletePet(pet: Pet) {
+        if (window.confirm("Deseja mesmo deletar esse registro de pet ?"))
+            this.petService.deletePet(pet).subscribe(() => {
+                this.getPets();
+            });
+    }
+
+    editPet(pet: Pet) {
+        this.pet = { ...pet };
+    }
+
+    cleanForm(form: NgForm) {
+        this.getPets();
+        form.resetForm();
+        this.pet = {} as Pet;
+    }
 }
